4fb9bbe621c0ae7bc13f099626ddee102beff304
Роман Бушуев <bushuev@benefitty.ru>
2024-05-05 14:38:02 +0400

    Initial commit

---
+++ CalcANTLR/Program.cs
@@ -0,0 +1,20 @@
+﻿using Antlr.Runtime;
+
+    try
+    {
+        // В качестве входного потока символов устанавливаем консольный ввод
+        ANTLRReaderStream input = new ANTLRReaderStream(Console.In);
+        // Настраиваем лексер на этот поток
+        CalcLexer lexer = new CalcLexer(input);
+        // Создаем поток токенов на основе лексера
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // Создаем парсер
+        CalcParser parser = new CalcParser(tokens);
+        // И запускаем первое правило грамматики!!!
+        parser.calc();      
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    Console.ReadKey();
